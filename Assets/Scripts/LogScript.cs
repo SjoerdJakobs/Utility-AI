e2d4f267340bfc6da0d0df0a2a0ea82419475e46
SjoerdJakobs <18284@ma-web.nl>
2016-10-11 15:16:28 +0200

    sync
    sync

---
+++ Assets/Scripts/LogScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("<color=blue>test</color>");
+	}
+}
---
+++ Assets/Scripts/PointsGiver.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class PointsGiver : MonoBehaviour {
+
+    private List<float> parameters = new List<float>(); 
+	
+    public PointType pointType = new PointType();
+
+    private Dictionary<PointTypeID, int> pointsToPointType = new Dictionary<PointTypeID, int>();
+
+    public void AddPointsToType(PointTypeID ptID, )
+    {
+        pointsToPointType.Add(ptID, );
+    }
+
+    public void AddType()
+    {
+
+    }
+
+    public void ReturnPoints()
+    {
+
+    }
+
+    
+}
